a11764d91ff4215e0d95ee3bf032250b5e206f8e
vanmanh621 <73931677+vanmanh621@users.noreply.github.com>
2020-11-05 17:54:45 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb://localhost:27017/newshop',
+        dbname : "newshop",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb://localhost:27017/newshop',
+        dbname : "newshop",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
